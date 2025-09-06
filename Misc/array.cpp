1cb00615456182419d52bfb4615085dbc5fc9471
Amit Yadav <amityadavv@outlook.in>
2025-09-06 18:21:50 +0530

    function

--- Misc/array.cpp
+++ Misc/array.cpp
@@ -28,21 +28,46 @@
 
 ///
 
-#include<iostream>
-using namespace std;
-void  printarray(int arr[],int size ){
-    cout<<"printing the array\n";
-    for(int i=0;i<size;i++){
-        cout<<arr[i] <<"\n";
-    }}
+// #include<iostream>
+// using namespace std;
+// void  printarray(int arr[],int size ){
+//     cout<<"printing the array\n";
+//     for(int i=0;i<size;i++){
+//         cout<<arr[i] <<"\n";
+//     }}
 
-    int main(){
-        int n=15;
-        int third[15]={1,2};
-        printarray(third ,10);
-    }
+//     int main(){
+//         int n=15;
+//         int third[15]={1,2};
+//         printarray(third ,10);
+//     }
+ //printing the array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int arr[10]={1,2,3,4,5,6,67,8,9,0};
+//     cout<<"Printing An Array \n";
+//     for(int i=0;i<10;i++){
+//         cout<<arr[i] <<endl;
+//     }
+//     return 0;
+// }
 
-
+//taking input in array
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int array[10];
+//     cout<<"Give input to the array \n";
+//     for(int i=0;i<10;i++){
+//         cin>>array[i];
+//     }
+//     cout<<"printing the array \n";
+//     for(int i=0;i<10;i++){
+//         cout<<array[i] <<endl;
+//     }
+//     return 0;
+// }
 
 
 
---
+++ Misc/tempCodeRunnerFile.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+// using namespace std;
+// int main(){
+//     int array[10];
+//     cout<<"Give input to the array \n";
+//     for(int i=0;i<10;i++){
+//         cin>>array[i];
+//     }
+//     cout<<"printing the array \n";
+//     for(int i=0;i<10;i++){
+//         cout<<array[i] <<endl;
+//     }
+//     return 0;
+// }
